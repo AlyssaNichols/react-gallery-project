490ae46a016e1de9af41c0bbeed01c378f232b7d
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-25 10:20:49 -0500

    just took out some unnecessary text in router functions

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); 
 const pool = require("../modules/pool");
 // const galleryItems = require('../modules/gallery.data');
 
@@ -10,8 +10,8 @@ router.put("/like/:id", (req, res) => {
   const queryText = `UPDATE "gallery" SET "likes"=("likes"+1) WHERE "id"=$1;`;
   pool
     .query(queryText, [id])
-    .then((results) => {
-      res.send(`Liked photo id of: ${id}`).status(200);
+    .then(() => {
+      res.sendStatus(204);
     })
     .catch((error) => {
       console.log("error caught in PUT /like: ", error);
@@ -57,7 +57,7 @@ router.delete("/delete/:id", (req, res) => {
   const queryText = `DELETE FROM "gallery" WHERE "id"=$1;`;
   pool
     .query(queryText, [id])
-    .then((response) => res.sendStatus(204))
+    .then(() => res.sendStatus(204))
     .catch((error) => {
       console.log("Error in DELETE with id", error);
       res.sendStatus(500);
